e233becc69b7c1a199b4df70871b53dec89622d4
Furkan Bülbül <furkanbulbul532@gmail.com>
2026-06-02 01:25:57 +0300

    docs: update readme with local allure reporting guidelines

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -3,11 +3,12 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(
-    features = "src/test/resources/features",
-    glue = "stepdefinitions",
-    plugin = {"pretty", "html:target/cucumber-reports.html"}
-)
+@CucumberOptions(features = "src/test/resources/features", glue = "stepdefinitions", plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber-html-report.html",
+        "json:target/cucumber-reports/cucumber.json",
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+}, monochrome = true)
 public class TestRunner extends AbstractTestNGCucumberTests {
-    
+
 }
